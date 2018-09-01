fd29560b7b6ee73017a6563690fc162ac5962ab4
Kevin Kabatra <kevin@lsretail.com>
2018-09-01 14:07:50 +0000

    Library modified to support interrupts
    The library no longer uses its own state machine to handle the button firing logic, instead I have begun to leverage interrupts in my sketch for this functionality. Included examples on how this code will be used in the sketch and added comments blocks for everything.

--- ArcadeButton.cpp
+++ ArcadeButton.cpp
@@ -1,177 +1,161 @@
+/**
+ * @Purpose 
+ *   Arduino library that will support physical arcade buttons.
+ * 
+ * @Description
+ *   Each instance of a button contains:
+ *   1. a human readable description, which can be used for debugging or 
+ *      other reporting purposes,
+ *   2. a digital pin that the button will send its input to,
+ *   3. and the ASCII Key Code that will represent the key that will be
+ *      sent to the Keyboard library.
+ * 
+ *  This libray can be used with or without an interrupt, but only provides
+ *  examples for setting up the library for usage with interrupts. One thing
+ *  to note, when using interrupts you must ensure that you handle the switch
+ *  debouncing if you are using a physical switch / button. 
+ * 
+ *  Debouncing can be solved using software, see 
+ *  https://www.arduino.cc/en/Tutorial/Debounce, or hardware, see
+ *  https://www.youtube.com/watch?v=tmjuLtiAsc0, to ensure the best
+ *  accuracy I chose to debounce using a Schmitt trigger as shown in the
+ *  video linked above. 
+ * 
+ *  Interrupts must be setup within the Arduino's sketch file otherwise
+ *  the interrupt will be unable to access the button's instance variables.
+ * 
+ * @Example
+ *  // Construct a single button
+ *  ArcadeButton player1Start("Player 1 Start", 2, 49);
+ * 
+ *  // Construct multiple buttons
+ *  const short numberOfArcadeButtons = 4;
+ *  ArcadeButton arcadeButtons[numberOfArcadeButtons] = {
+ *    ArcadeButton("Player 1 Start", 2, 49),
+ *    ArcadeButton("Player 2 Start", 3, 50),
+ *    ArcadeButton("Player 1 Coin" , 4, 51),
+ *    ArcadeButton("Player 2 Coin" , 5, 52)
+ *  };
+ * 
+ * // Setup interrupt
+ *  attachInterrupt(
+ *    digitalPinToInterrupt(arcadeButtons[0].GetDigitalPin()),
+ *    []() {
+ *          if(digitalRead(arcadeButtons[0].GetDigitalPin())) {
+ *            Keyboard.begin();
+ *            Keyboard.press(arcadeButtons[0].GetASCIIKeyCode());            
+ *          }
+ *          else {
+ *            Keyboard.release(arcadeButtons[0].GetASCIIKeyCode());
+ *            Keyboard.end();
+ *          }
+ *    },
+ *    CHANGE
+ *  );   
+ * 
+ * @LICENSE 
+ *   MIT License
+ *   Copyright (c) 2018 Kevin Kabatra
+ * 
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ * 
+ *   The above copyright notice and this permission notice shall be included in all
+ *   copies or substantial portions of the Software.
+ * 
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *   SOFTWARE.
+*/
+
 #include "Arduino.h"
 #include "ArcadeButton.h"
 
-/*
-  Constructor
-*/
-ArcadeButton::ArcadeButton(int number, String description, int digitalPin) {
-  _description = description;
-  _digitalPin = digitalPin;
-  _number = number;
-  
-  pinMode(_digitalPin, INPUT);
-  init();
+/**
+ * Constructor for an arcade button that will send a signal to the Arduino.
+ * 
+ * @param description String representing a human readable name or description
+ *  of the physical button. This is usefull for debugging or other reporting
+ *  purposes.
+ * @param digitalPin Byte representing the physical digital pin location on the
+ *  Arduino that this button's input is sent to.
+ * @param asciiKeyCode Byte representing an ASCII Key Code that will be sent to
+ *  the Keyboard library when the button is pressed / released.
+ */
+ArcadeButton::ArcadeButton(
+  String description,
+  byte digitalPin, 
+  byte asciiKeyCode
+) {
+  SetASCIIKeyCode(asciiKeyCode);
+  SetDescription(description);
+  SetDigitalPin(digitalPin);  
 }
 
-ArcadeButton::ArcadeButton(int number, String description, int digitalPin, int asciiKeyCode) {
-  _asciiKeyCode = asciiKeyCode;
-  _description = description;
-  _digitalPin = digitalPin;
-  _number = number;
-  
-  pinMode(_digitalPin, INPUT);
-  init();
-}
-
-void ArcadeButton::init() {
-  _buttonState = ArcadeButton::States::UnPressed;
-}
-
-int ArcadeButton::GetButtonASCIIKeyCode() {
+/**
+ * Returns the ASCII Key Code that was assigned to this button.
+ * 
+ * @return Byte representing an ASCII Key Code that will be sent to
+ *  the Keyboard library when the button is pressed / released.
+ */
+byte ArcadeButton::GetASCIIKeyCode() {
   return _asciiKeyCode;
 }
+/**
+ * Sets the ASCII Key Code for this button.
+ * 
+ * @param asciiKeyCode Byte representing an ASCII Key Code that will be sent to
+ *  the Keyboard library when the button is pressed / released.
+ */
+void ArcadeButton::SetASCIIKeyCode(byte& asciiKeyCode) {
+  _asciiKeyCode = asciiKeyCode;
+}
 
-String ArcadeButton::GetButtonDescription() {
+/**
+ * Returns the human readable description assigned to this button.
+ * 
+ * @return String representing a human readable name or description
+ *  of the physical button. This is usefull for debugging or other reporting
+ *  purposes.
+ */
+String ArcadeButton::GetDescription() {
   return _description;
 }
+/**
+ * Sets the description for this button.
+ * 
+ * @param description String representing a human readable name or description
+ *  of the physical button. This is usefull for debugging or other reporting
+ *  purposes.
+ */
+void ArcadeButton::SetDescription(String& description) {
+  _description = description;
+}
 
-int ArcadeButton::GetButtonDigitalPin() {
+/**
+ * Returns the digital pin assigned to this button.
+ * 
+ * @return Byte representing the physical digital pin location on the
+ *  Arduino that this button's input is sent to.
+ */
+byte ArcadeButton::GetDigitalPin() {
   return _digitalPin;
 }
-
-int ArcadeButton::GetButtonNumber() {
-  return _number;
-}
-
-ArcadeButton::States ArcadeButton::GetButtonState() {
-  return _buttonState;
-}
-
-void ArcadeButton::SetButtonStatePressed() {
-  TransitionButtonState(ReadInputPinsPressed());
-}
-
-void ArcadeButton::SetButtonStateHeld() {
-  TransitionButtonState(ReadInputPinsHeld());
-}
-
-/*
-  Attempts to transition _buttonState to the desired state.
-
-  This function works as a mini-State Machine in that it governs the
-  ability states that a button can transition to and from.
-
-  Due to the mechanical nature of a button it is impossible for a 
-  particular state to transition to itself so all re-entry
-  transitions are denied.
-
-  returns: Boolean representing if the button was transitioned.
-*/
-bool ArcadeButton::TransitionButtonState(ArcadeButton::States transitionTo) {
-  bool isTransitionSuccessful = false;
-
-  switch(transitionTo) {
-    /*
-      Transition to ArcadeButton::States::UnPressed
-
-      Permitted Transitions:
-        Pressed -> UnPressed
-        Held    -> UnPressed
-      Denied Transitions:
-        UnPressed -> UnPressed
-    */
-    case ArcadeButton::States::UnPressed:
-      if(_buttonState == ArcadeButton::States::UnPressed) {
-        isTransitionSuccessful = false;
-      }
-      else {
-        _buttonState = transitionTo;
-        isTransitionSuccessful = true;
-      }
-      break;
-
-    /*
-      Transition to ArcadeButton::States::Pressed
-
-      Permitted Transitions:
-        UnPressed -> Pressed
-      Denied Transitions:
-        Pressed -> Pressed
-        Held    -> Pressed
-    */
-    case ArcadeButton::States::Pressed:
-      if(_buttonState == ArcadeButton::States::Pressed ||
-         _buttonState == ArcadeButton::States::Held) {
-        isTransitionSuccessful = false;        
-      }      
-      else if(_buttonState == ArcadeButton::States::UnPressed) {
-        _buttonState = transitionTo;
-        isTransitionSuccessful = true;
-      }
-      break;
-
-    /*
-      Transition to ArcadeButton::States::Held
-
-      Permitted Transitions:
-        Pressed -> Held
-      Denied Transitions:
-        Unpressed -> Held
-        Held      -> Held
-    */
-    case ArcadeButton::States::Held:
-      if(_buttonState == ArcadeButton::States::UnPressed ||
-         _buttonState == ArcadeButton::States::Held) {
-        isTransitionSuccessful = false;        
-      }      
-      else if(_buttonState == ArcadeButton::States::Pressed) {
-        _buttonState = transitionTo;
-        isTransitionSuccessful = true;
-      }    
-      break;
-  }
-
-  return isTransitionSuccessful;
-}
-
-/*
-  Reads the Digital Pin associated to the Arcade Button
-
-  returns: ArcadeButton::States representing the value
-  that is read from the pin as it relates to the Arcade Button's
-  current action.
-*/
-ArcadeButton::States ArcadeButton::ReadInputPinsPressed() {
-  if(digitalRead(_digitalPin) != 0) {
-    //Was the button held since the last loop?
-    if(_buttonState == ArcadeButton::States::Pressed || 
-      _buttonState == ArcadeButton::States::Held
-    )
-    {
-      return ArcadeButton::States::Held;
-    }    
-    else {
-      return ArcadeButton::States::Pressed;  
-    }
-  }
-  else {
-    return ArcadeButton::States::UnPressed;
-  }
-}
-
-/*
-  Reads the Digital Pin associated to the Arcade Button
-
-  returns: ArcadeButton::States representing the value
-  that is read from the pin as it relates to the Arcade Button's
-  current action.
-*/
-ArcadeButton::States ArcadeButton::ReadInputPinsHeld() {
-  if(digitalRead(_digitalPin) != 0) {
-    //Was the button held since the last loop?
-    if(_buttonState == ArcadeButton::States::Pressed) {
-      return ArcadeButton::States::Held;
-    }
-  }
-  return _buttonState;
+/**
+ * Sets the digital pin for this button.
+ * 
+ * @param digitalPin Byte representing the physical digital pin location on the
+ *  Arduino that this button's input is sent to.
+ */
+void ArcadeButton::SetDigitalPin(byte& digitalPin) {
+  _digitalPin = digitalPin;
+  pinMode(_digitalPin, INPUT);
 }
--- ArcadeButton.h
+++ ArcadeButton.h
@@ -1,5 +1,79 @@
-/*
-  Arduino library that will support physical arcade buttons.
+/**
+ * @Purpose 
+ *   Arduino library that will support physical arcade buttons.
+ * 
+ * @Description
+ *   Each instance of a button contains:
+ *   1. a human readable description, which can be used for debugging or 
+ *      other reporting purposes,
+ *   2. a digital pin that the button will send its input to,
+ *   3. and the ASCII Key Code that will represent the key that will be
+ *      sent to the Keyboard library.
+ * 
+ *  This libray can be used with or without an interrupt, but only provides
+ *  examples for setting up the library for usage with interrupts. One thing
+ *  to note, when using interrupts you must ensure that you handle the switch
+ *  debouncing if you are using a physical switch / button. 
+ * 
+ *  Debouncing can be solved using software, see 
+ *  https://www.arduino.cc/en/Tutorial/Debounce, or hardware, see
+ *  https://www.youtube.com/watch?v=tmjuLtiAsc0, to ensure the best
+ *  accuracy I chose to debounce using a Schmitt trigger as shown in the
+ *  video linked above. 
+ * 
+ *  Interrupts must be setup within the Arduino's sketch file otherwise
+ *  the interrupt will be unable to access the button's instance variables.
+ * 
+ * @Example
+ *  // Construct a single button
+ *  ArcadeButton player1Start("Player 1 Start", 2, 49);
+ * 
+ *  // Construct multiple buttons
+ *  const short numberOfArcadeButtons = 4;
+ *  ArcadeButton arcadeButtons[numberOfArcadeButtons] = {
+ *    ArcadeButton("Player 1 Start", 2, 49),
+ *    ArcadeButton("Player 2 Start", 3, 50),
+ *    ArcadeButton("Player 1 Coin" , 4, 51),
+ *    ArcadeButton("Player 2 Coin" , 5, 52)
+ *  };
+ * 
+ * // Setup interrupt
+ *  attachInterrupt(
+ *    digitalPinToInterrupt(arcadeButtons[0].GetDigitalPin()),
+ *    []() {
+ *          if(digitalRead(arcadeButtons[0].GetDigitalPin())) {
+ *            Keyboard.begin();
+ *            Keyboard.press(arcadeButtons[0].GetASCIIKeyCode());            
+ *          }
+ *          else {
+ *            Keyboard.release(arcadeButtons[0].GetASCIIKeyCode());
+ *            Keyboard.end();
+ *          }
+ *    },
+ *    CHANGE
+ *  );   
+ * 
+ * @LICENSE 
+ *   MIT License
+ *   Copyright (c) 2018 Kevin Kabatra
+ * 
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ * 
+ *   The above copyright notice and this permission notice shall be included in all
+ *   copies or substantial portions of the Software.
+ * 
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *   SOFTWARE.
 */
 
 #ifndef ArcadeButton_h
@@ -9,34 +83,29 @@
 
 class ArcadeButton {
   public:
-    /* Constructor */
-    ArcadeButton(int number, String description, int digitalPin);
-    ArcadeButton(int number, String description, int digitalPin, int asciiKeyCode);
+    /* The button */
+    ArcadeButton(
+      String description,
+      byte digitalPin, 
+      byte asciiKeyCode, 
+    );
 
-    enum States { UnPressed, Pressed, Held };
+    /* Extended ASCII Codes have up to 255 key code, so byte covers this range for us */
+    byte GetASCIIKeyCode();
+    void SetASCIIKeyCode(byte& asciiKeyCode);
 
-    void init();
-    void SetButtonStatePressed();
-    void SetButtonStateHeld();
-    
-    // Getter methods
-    int    GetButtonASCIIKeyCode();
-    String GetButtonDescription();
-    int    GetButtonDigitalPin();
-    int    GetButtonNumber();
-    States GetButtonState();
+    /* Description is useful for debugging purposes, but not for anything else */
+    String GetDescription();
+    void   SetDescription(String& description);
+
+    /* Arduinos do not have enough pins to fill up a byte. */
+    byte GetDigitalPin();
+    void SetDigitalPin(byte& digitalPin);
 
   private:
-    States ReadInputPinsPressed();
-    States ReadInputPinsHeld();
-    bool TransitionButtonState(States transitionTo);
-
-    int _number;
+    byte   _asciiKeyCode; //A byte stores an 8-bit unsigned number, from 0 to 255.
     String _description;
-    int _digitalPin;
-    int _asciiKeyCode;
-
-    States _buttonState;
+    byte   _digitalPin;
 };
 
 #endif
